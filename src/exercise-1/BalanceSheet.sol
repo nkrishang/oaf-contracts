0e3b13980d4ae37954ae0af1facc4cf74a991b1b
Krishang <krishang@thirdweb.com>
2023-04-06 11:49:29 -0400

    init exercise-1

---
+++ src/exercise-1/BalanceSheet.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/**
+ *  Exercise: Write a smart contract that does the following:
+ *      1. Maintains a balance sheet of addresses -> to amounts.
+ *      2. Gives the deployer of the contract a balance of 10_000 at the time of contract creation.
+ *      3. Allows an address to transfer its balance to another address.
+**/
+
+contract BalanceSheet {
+    constructor() {}
+}
