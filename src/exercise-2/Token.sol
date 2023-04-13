ab6c02a02e740a1639ae657758551995d11a4f5b
Krishang <krishang@thirdweb.com>
2023-04-13 11:51:42 -0400

    Exercise 2

---
+++ src/exercise-2/Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./IERC20.sol";
+
+/**
+ *  Exercise: Write a smart contract that implements the ERC-20 standard.
+ *            The contract should create an initial supply of 1_000_000 tokens at contract creation.
+**/
+
+contract Token is IERC20 {
+    constructor() {}
+}
