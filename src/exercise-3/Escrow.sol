dd6b6781124b68d8c7496062556387552e1e473f
Krishang <krishang@thirdweb.com>
2023-04-18 11:33:40 -0400

    Update exercise 3

--- src/exercise-3/Escrow.sol
+++ src/exercise-3/Escrow.sol
@@ -2,12 +2,27 @@
 pragma solidity ^0.8.13;
 
 /**
- *  Exercise: Write an ESCROW smart contract that does the following:
- *      - In the constructor:
- *              - let the deployer deposit an amount of ether.
- *              - store escrow details: the address of the other participant, and the expected currency + amount
- *                this participant must deposit to release the ether stored in the contract.
+ *  Exercise: Bob and Alice are entering an escrow. Alice wants to exchange 10 A-Tokens for 1 ether from Bob.
+ *            Alice and Bob don't trust each other, so they use a smart contract to execute this exchange.
  *
- *      - Allow the other participant to execute the escrow by by sending the expected currency + amount to
- *        the contract deployer, and receive the ether stored by the contract -- all in ONE transaction.
+ *            [1] Write an ESCROW smart contract that does the following:
+ *
+ *              - In the constructor:
+ *                  - Let Bob deposit a given amount of ether into the smart contract.
+ *                  - Store escrow details: 
+ *                      (a) the address of Alice 
+ *                      (b) the expected currency + amount that Alice must deposit to get the ether stored in the contract by Bob
+ *                      (c) the amount of time Bob is willing to wait for Alice to execute the escrow.
+ *
+ *              - Allow Alice to execute the escrow by by sending the expected currency + amount to the contract,
+ *                to receive the stored ether in exchange.
+ *
+ *              - If Alice hasn't executed the escrow within the time that Bob expects, let Bob withdraw the ether stored in the contract.
+ *
+ *            [2] Since Alice and Bob don't trust each other, they want to make sure that the smart contract
+ *                is not malicious. So -- deploy the smart contract to the Goerli blockchain and verify it.
+ *
+ *  HINT: You can use the IERC20 contract to represent the A-Tokens.
 **/
+
+contract Escrow {}
